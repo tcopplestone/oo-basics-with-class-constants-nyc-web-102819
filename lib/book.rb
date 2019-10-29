61575bfca2d1e80fca0786e4036190299c6f1b5f
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-29 18:53:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,14 +7,14 @@ class Book
   def initialize(title)
     @title = title
   end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
   
   def genre=(genre)
     @genre = genre
     GENRES << genre
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
   end
 
 end
