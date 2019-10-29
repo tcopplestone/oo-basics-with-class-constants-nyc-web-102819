40ee713bf4ead9ee9aa8ba0089379c05d8126200
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-29 19:04:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |n|
-      if n != brand
-        BRANDS << brand
-      end
-    end
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
